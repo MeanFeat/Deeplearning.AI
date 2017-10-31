dc22edf7df58ea7bd1375d7ad32d84ea830578c9
Unknown <jlecoui@msn.com>
2017-10-30 18:13:21 -0700

    SDL2 Setup; binary data

--- Week2LogisticRegressionCats/LogReg.cpp
+++ Week2LogisticRegressionCats/LogReg.cpp
@@ -4,6 +4,10 @@
 #include <fstream>
 #include <stdlib.h>
 #include <stdio.h>
+#include <SDL.h>
+
+#define WINWIDTH 800
+#define WINHEIGHT 600
 
 struct LogRegSet {
 	MatrixXf w;
@@ -31,9 +35,9 @@ MatrixXf GetModelOutput(LogRegSet set, MatrixXf X) {
 LRTrainingSet propegate(LogRegSet set, MatrixXf X, MatrixXf Y) {
 	LRTrainingSet Result;
 	int m = (int)Y.cols();
-	float coeff = 1.f/ m;
+	float coeff = 1.f / m;
 	MatrixXf A = GetModelOutput(set, X);
-	Result.Cost = -((Y.cwiseProduct(Log(A))) + (MatrixXf::Ones(1, m) - Y).cwiseProduct( (Log(MatrixXf::Ones(1, m) - A)))).sum() * coeff;
+	Result.Cost = -((Y.cwiseProduct(Log(A))) + (MatrixXf::Ones(1, m) - Y).cwiseProduct((Log(MatrixXf::Ones(1, m) - A)))).sum() * coeff;
 	Result.dw = (X * (A - Y).transpose()) * coeff;
 	Result.db = (A - Y).sum()*coeff;
 	return Result;
@@ -43,93 +47,117 @@ MatrixXf predict(LogRegSet lrs, MatrixXf X) {
 	return (GetModelOutput(lrs, X)).array().round();
 }
 
-void optimize(LogRegSet *lrsPtr, LRTrainingSet *trainSetPtr, MatrixXf X, MatrixXf Y, int iterations, float learnRate) {
+void drawLine(SDL_Renderer *ren, int x, int y, int x1, int y1) {
+	int dx = x1 - x;
+	int dy = y1 - y;
+	if(dx == 0 || dy == 0) {
+		//return;
+	}
+	if(abs(dx) > abs(dy) && dx) {
+		if(dx) dy /= abs(dx);
+		else dy = 0;
+		if(dx >= 0) dx = 1;
+		else dx = -1;
+		do { //for(;x<x1; x++){
+			SDL_RenderDrawPoint(ren, x, y);
+			y += dy;
+			x += dx;
+		} while(x != x1);
+	} else if(dy) {
+		if(dy) dx /= abs(dy);
+		else dx = 0;
+		if(dy > 0) dy = 1;
+		else dy = -1;
+		do { //for(;y<y1; y++){
+			SDL_RenderDrawPoint(ren, x, y);
+			x += dx;
+			y += dy;
+		} while(y != y1);
+	}
+}
+
+void optimize(SDL_Renderer *ren, LogRegSet *lrsPtr, LRTrainingSet *trainSetPtr, MatrixXf X, MatrixXf Y, int iterations, float learnRate) {
+	vector<float> Xplots;
+	vector<float> tXplots;
+	SDL_SetRenderDrawColor(ren, 0, 0, 0, 0);
+	SDL_RenderClear(ren);
 	for(int i = 0; i < iterations; i++) {
 		*trainSetPtr = propegate(*lrsPtr, X, Y);
 		lrsPtr->w = lrsPtr->w - (learnRate * trainSetPtr->dw);
 		lrsPtr->b = lrsPtr->b - (learnRate * trainSetPtr->db);
 		Assert(trainSetPtr->dw.rows() == lrsPtr->w.rows() && trainSetPtr->dw.cols() == lrsPtr->w.cols());
-		if(i % 100 == 0) {
-			cout << "Cost: " << (float)trainSetPtr->Cost << " //==// train accuracy: " << 100 - ((predict(*lrsPtr, X) - Y).array().abs().sum() / Y.cols() * 100) << "%\n";
+		if(i % 2 == 0) {
+			SDL_SetRenderDrawColor(ren, 0, 0, 0, 0);
+			SDL_RenderClear(ren);
+			Xplots.push_back(((predict(*lrsPtr, X) - Y).array().abs().sum() / Y.cols() * WINHEIGHT));
+			int slider = Xplots.size() >= WINWIDTH ? Xplots.size() - WINWIDTH : 0;
+			for(int p = slider + 1; p < (int)Xplots.size(); p++) {
+				SDL_SetRenderDrawColor(ren, 0, 0, 255, 255);
+				drawLine(ren, (p - 1) - slider, int(Xplots[p - 1]), p - slider, int(Xplots[p]));
+			}
+			SDL_RenderPresent(ren);
 		}
 	}
 }
 
-MatrixXf BuildMatFromFile(string fName, MatrixXf mat) {
-	cout << "Loading File : " << fName;
-	MatrixXf tempMat = mat;
-	ifstream file(fName);
-	for(int row = 0; row < mat.rows(); ++row) {
-		std::string line;
-		std::getline(file, line);
-		if(!file.good())
-			break;
-		std::stringstream iss(line);
-		for(int col = 0; col < mat.cols(); ++col) {
-			std::string val;
-			std::getline(iss, val, ',');
-			if(!iss.good())
-				break;
-			std::stringstream convertor(val);
-			convertor>> tempMat(row,col);
-		}
-	}
-	cout << " : 100.0%" << endl;
-	return tempMat;
-}
-
-void PrintBool(bool inp) {
-	if(inp) {
-		cout << "             00 " << endl;
-		cout << "            00  " << endl;
-		cout << "           00   " << endl;
-		cout << "00       000    " << endl;
-		cout << "  00    00      " << endl;
-		cout << "    00 00       " << endl;
-		cout << "     000        " << endl;
-	} else {
-		cout << "  XX     XX       " << endl;
-		cout << "   XX   XX        " << endl;
-		cout << "    XX XX         " << endl;
-		cout << "     XXX           " << endl;
-		cout << "    XX XX          " << endl;
-		cout << "   XX   XX         " << endl;
-		cout << "  XX     XX        " << endl;
-	}
-}
-
-int main() {
+void LogisticRegression(SDL_Renderer *ren) {
 	LogRegSet lrs = InitToZero(12288);
-	MatrixXf train_set_x = BuildMatFromFile("catTrain.csv", MatrixXf::Zero(12288, 209));
-	MatrixXf test_set_x = BuildMatFromFile("catTest.csv", MatrixXf::Zero(12288, 50));
-	MatrixXf train_set_y = BuildMatFromFile("catTrainLabels.csv", MatrixXf::Zero(1, 209));
-	MatrixXf test_set_y = BuildMatFromFile("catTestLabels.csv", MatrixXf::Zero(1, 50));
+	MatrixXf train_set_x;
+	read_binary("catTrain.dat", train_set_x);
+	MatrixXf test_set_x;
+	read_binary("catTest.dat", test_set_x);
+	MatrixXf train_set_y;
+	read_binary("catTrainLabels.dat", train_set_y);
+	MatrixXf test_set_y;
+	read_binary("catTestLabels.dat", test_set_y);
 	Assert(train_set_x.rows() == 12288 && train_set_x.cols() == 209);
 	Assert(train_set_y.rows() == 1 && train_set_y.cols() == 209);
 	Assert(test_set_x.rows() == 12288 && test_set_x.cols() == 50);
 	Assert(test_set_y.rows() == 1 && test_set_y.cols() == 50);
 	LRTrainingSet lrts;
-	optimize(&lrs, &lrts, train_set_x, train_set_y, 1500, 0.005f);
-	cout << "//=============================//" << endl;
-	cout << "train accuracy: " << 100 - ((predict(lrs, train_set_x) - train_set_y).array().abs().sum() / train_set_x.cols() * 100) << endl;
-	cout << "test accuracy: " << 100 - ((predict(lrs, test_set_x) - test_set_y).array().abs().sum() / test_set_x.cols() * 100) << endl;
+	optimize(ren, &lrs, &lrts, train_set_x, train_set_y, 1500, 0.005f);
 
+	SDL_SetRenderDrawColor(ren, 0, 0, 0, 0);
+	SDL_RenderClear(ren);
 	int imageIndex = 0;
+	int offset = 0;
+	int offsetIndex = 0;
 	auto preds = predict(lrs, test_set_x);
-	while(imageIndex<50) {
-		if(preds(imageIndex) != test_set_y(imageIndex) ){
+	while(imageIndex < test_set_y.cols()) {
+		if(preds(imageIndex) != test_set_y(imageIndex)) {
 			for(int x = 0; x < 64; x++) {
-				for(int y = 0; y < 192; y++) {
-					float pix = test_set_x(x * 192 + y, imageIndex);
-					cout << (pix < 0.1f ? ' ' : pix < 0.2f ? '.' : pix < 0.3f ? '-' : pix < 0.4f ? '+' : pix < 0.5f ? '*' : pix < 0.75f ? 'X' : '#');
+				for(int y = 0; y < 192; y += 3) {
+					SDL_SetRenderDrawColor(ren, test_set_x(x * 192 + y, imageIndex) * 255,
+										   test_set_x(x * 192 + y + 1, imageIndex) * 255,
+										   test_set_x(x * 192 + y + 2, imageIndex) * 255,
+										   255);
+					SDL_RenderDrawPoint(ren, offset + y / 3, offsetIndex * 64 + x);
 				}
-				cout << endl;
 			}
-			PrintBool(preds(imageIndex) == 1.f);
-			PrintBool(test_set_y(imageIndex) == 1.f);
+			offsetIndex++;
 		}
 		imageIndex++;
-	}		
-	cout << "Press a key to continue...";
-	getchar();
-}	
+		if(offsetIndex >= 8) {
+			offset += 70;
+			offsetIndex = 0;
+		}
+	}
+	SDL_RenderPresent(ren);
+}
+
+int main(int argc, char *argv[]) {
+	SDL_Window *win;
+	SDL_Renderer *ren;
+	SDL_Init(SDL_INIT_VIDEO);
+	SDL_CreateWindowAndRenderer(WINWIDTH, WINHEIGHT, 0, &win, &ren);
+	LogisticRegression(ren);
+	SDL_Event localEvent;
+	SDL_PollEvent(&localEvent);
+	while(localEvent.type != SDL_MOUSEBUTTONDOWN) {
+		SDL_PollEvent(&localEvent);
+	}
+	SDL_DestroyRenderer(ren);
+	SDL_DestroyWindow(win);
+	SDL_Quit();
+	return 0;
+}
--- Week2LogisticRegressionCats/stdafx.h
+++ Week2LogisticRegressionCats/stdafx.h
@@ -9,12 +9,34 @@
 #include <stdio.h>
 #include <tchar.h>
 #include "stdMat.h"
-#include <chrono>
+#include "SDL.h"
 
 #define Assert(Expression) if(!(Expression)) {*(int *)0 = 0;}
 
 using namespace Eigen;
 using namespace std;
-using namespace std::chrono;
+
+
+namespace Eigen {
+	template<class Matrix>
+	void write_binary(const char* filename, const Matrix& matrix) {
+		std::ofstream out(filename, ios::out | ios::binary | ios::trunc);
+		typename Matrix::Index rows = matrix.rows(), cols = matrix.cols();
+		out.write((char*)(&rows), sizeof(typename Matrix::Index));
+		out.write((char*)(&cols), sizeof(typename Matrix::Index));
+		out.write((char*)matrix.data(), rows*cols * sizeof(typename Matrix::Scalar));
+		out.close();
+	}
+	template<class Matrix>
+	void read_binary(const char* filename, Matrix& matrix) {
+		std::ifstream in(filename, ios::in | std::ios::binary);
+		typename Matrix::Index rows = 0, cols = 0;
+		in.read((char*)(&rows), sizeof(typename Matrix::Index));
+		in.read((char*)(&cols), sizeof(typename Matrix::Index));
+		matrix.resize(rows, cols);
+		in.read((char *)matrix.data(), rows*cols * sizeof(typename Matrix::Scalar));
+		in.close();
+	}
+}
 
 // TODO: reference additional headers your program requires here
