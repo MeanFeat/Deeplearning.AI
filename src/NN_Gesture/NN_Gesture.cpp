916525a44b727fc3a44f8ffb70e5235f860a8993
Master <jlecoui@msn.com>
2020-07-25 17:35:15 -0700

    Delta capture; Multiply data; Transpose exports;

--- src/NN_Gesture/NN_Gesture.cpp
+++ src/NN_Gesture/NN_Gesture.cpp
@@ -23,19 +23,34 @@ global_variable vector<Vector2f> labels;
 global_variable bool isCapturingEight = false;
 
 global_variable float *verify;
+global_variable float *verifyLabel;
 
 float mouseX = WINHALFWIDTH;
 float mouseY = WINHALFHEIGHT;
 
-Net neural;
+Net neural;						
 Buffer backBuffer;
 
 void RecordSample(float label){
+	vector<Vector2f> mods;
+	mods.push_back(Vector2f(1.f, 1.f));
+	mods.push_back(Vector2f(-1.f, 1.f));
+	mods.push_back(Vector2f(1.f, -1.f));
+	mods.push_back(Vector2f(-1.f, -1.f));
 	vector<Vector2f> sample = mouseCapture;
 	vector<Vector2f> delta = deltaCapture;
-	samples.push_back(sample);
-	deltas.push_back(delta);
-	labels.push_back(Vector2f(label, 0.f));
+	for (int c = 0; c < mods.size(); c++) {
+		vector<Vector2f> tempSample = sample;
+		vector<Vector2f> tempDelta = delta;
+		for( int i = 0; i < MAXCAPTURECOUNT; i++ ) {
+			tempSample[i] = Vector2f(tempSample[i][0] * mods[c][0] + ( mods[c][0] > 0 ? 0 : WINHEIGHT),
+									 tempSample[i][1] * mods[c][1] + ( mods[c][1] > 0 ? 0 : WINWIDTH ));
+			tempDelta[i] = Vector2f(tempDelta[i][0] * mods[c][0], tempDelta[i][1] * mods[c][1] );
+		}
+		samples.push_back(tempSample);
+		deltas.push_back(tempDelta);
+		labels.push_back(Vector2f(label, 0.f));
+	}
 }
 
 internal LRESULT CALLBACK Win32MainWindowCallback(HWND Window, UINT Message, WPARAM WParam, LPARAM LParam) {
@@ -56,11 +71,17 @@ internal LRESULT CALLBACK Win32MainWindowCallback(HWND Window, UINT Message, WPA
 		switch( WParam ) {
 			case '8':
 			isCapturingEight = false;
+			samples.erase(samples.end() - MAXCAPTURECOUNT, samples.end());
+			deltas.erase(deltas.end() - MAXCAPTURECOUNT, deltas.end());
+			labels.erase(labels.end() - MAXCAPTURECOUNT, labels.end());
 			RecordSample(1.f);
 			break; 
 			case 'V': //fall through
 			*verify = 1.f;
+			skipStep = true;
+			break;
 			case 'C':
+			*verifyLabel = 0.f;
 			skipStep = true;
 			break;
 		}
@@ -147,10 +168,21 @@ void ContainVector(vector<Vector2f> &vec, int maxSize) {
 	}
 }
 
+void CopyNestedVec(MatrixXf &saveData, vector<vector<Vector2f>> * s, int maxCount) {
+	int k = 0;
+	for( int i = 0; i < saveData.cols(); i++ ) {
+		for( int j = 0; j < maxCount; j++ ) {
+			*( saveData.data() + k++ ) = float(( *s )[i][j][0]);
+			*( saveData.data() + k++ ) = float(( *s )[i][j][1]);
+		}
+	}
+}
+
 int CALLBACK WinMain(HINSTANCE Instance, HINSTANCE PrevInstance, LPSTR CommandLine, int ShowCode) {
-	MatrixXf readSamples = BuildMatFromFile("GestureData.csv");
-	MatrixXf readDeltas = BuildMatFromFile("GestrureDeltas.csv");
-	MatrixXf readLabels = BuildMatFromFile("GestureLabels.csv");
+	MatrixXf readSamples = BuildMatFromFile("GestureSamples.csv").transpose();
+	MatrixXf readDeltas = BuildMatFromFile("GestureDeltas.csv").transpose();
+	MatrixXf readLabels = BuildMatFromFile("GestureLabels.csv").transpose();
+	assert(readSamples.size() == readDeltas.size());
 
 	WNDCLASSA winClass = {};
 	InitializeWindow(&winClass, Instance, Win32MainWindowCallback, &backBuffer, WINWIDTH, WINHEIGHT, "NN_PredictRadian");
@@ -167,36 +199,36 @@ int CALLBACK WinMain(HINSTANCE Instance, HINSTANCE PrevInstance, LPSTR CommandLi
 		//Main Loop
 		while(globalRunning) {
 			Win32ProcessPendingMessages();	
-			if( isVerifying && steps < readLabels.cols() && readLabels.cols() > 0 ) {
-				verify = &readLabels(1, steps);
-				if(!skipStep && readLabels(0, steps) == 1.f && readLabels(1, steps) == 0.f) {
-					for( int i = 0; i < readSamples.rows(); ) {
-						float valX = *( readSamples.data() + ( steps * readSamples.rows() + i++ ) );
-						float valY = *( readSamples.data() + ( steps * readSamples.rows() + i++ ) );						 
-						DrawFilledCircle(backBuffer, valX, WINHEIGHT - valY, 10.f, Color(100, 200, 0, 0));
+			if( isVerifying){
+				if( steps < readLabels.cols() && readLabels.cols() > 0 ) {
+					verifyLabel = &readLabels(0, steps);
+					verify = &readLabels(1, steps);
+					if( !skipStep && readLabels(0, steps) == 1.f && readLabels(1, steps) == 0.f ) {
+						for( int i = 0; i < readSamples.rows(); ) {
+							float valX = *( readSamples.data() + ( steps * readSamples.rows() + i++ ) );
+							float valY = *( readSamples.data() + ( steps * readSamples.rows() + i++ ) );
+							DrawFilledCircle(backBuffer, valX, WINHEIGHT - valY, 10.f, Color(i * 10, 200, 0, 0));
+						}
+					} else {
+						skipStep = false;
+						ClearScreen(backBuffer);
+						steps++;
 					}
-				} else {
-					skipStep = false;
-					ClearScreen(backBuffer);
-					steps++;
+					Win32DisplayBufferInWindow(deviceContext, window, backBuffer);
+					continue;
 				}
-				Win32DisplayBufferInWindow(deviceContext, window, backBuffer);
-				continue;
+				globalRunning = false;
 			}
-
 			if( mouseCapture.size() == 0 ) {
 				mouseCapture.push_back(Vector2f(float(mouseX), float(mouseY)));
 			}
-
 			ContainVector(mouseTrail, MOUSETRAILLENGTH);
-
 			mouseTrail.push_back(Vector2f(float(mouseX), float(mouseY)));
-
 			Vector2f end = mouseTrail.back();
 			Vector2f delta = mouseCapture.back() - end;
 			float dist = sqrtf(delta[0] * delta[0] + delta[1] * delta[1]);
 			if( dist >= CAPTURETHRESHOLD ){
-				deltaCapture.push_back(delta);
+				deltaCapture.push_back(delta/CAPTURETHRESHOLD);
 				mouseCapture.push_back(end);
 				ContainVector(mouseCapture, MAXCAPTURECOUNT);
 				ContainVector(deltaCapture, MAXCAPTURECOUNT);
@@ -208,39 +240,38 @@ int CALLBACK WinMain(HINSTANCE Instance, HINSTANCE PrevInstance, LPSTR CommandLi
 
 			UpdateDisplay(mouseTrail, mouseCapture);
 			Win32DisplayBufferInWindow(deviceContext, window, backBuffer);
-			
 			UpdateWinTitle(steps, window);
 		}
 
 		if( !isVerifying ) {
-			MatrixXf saveData = MatrixXf(MAXCAPTURECOUNT * 2, samples.size() - 1);
+			MatrixXf saveSamples = MatrixXf(MAXCAPTURECOUNT * 2, samples.size() - 1);
+			MatrixXf saveDeltas = MatrixXf(MAXCAPTURECOUNT * 2, deltas.size() - 1);
 			MatrixXf saveLabels = MatrixXf(2,samples.size() - 1);
-			int k = 0;
-			for( int i = 0; i < saveData.cols(); i++ ) {
-				for( int j = 0; j < MAXCAPTURECOUNT; j++ ) {
-					*( saveData.data() + k++ ) = float(samples[i][j][0]);
-					*( saveData.data() + k++ ) = float(samples[i][j][1]);
-				}
-			}
+			CopyNestedVec(saveSamples, &samples, MAXCAPTURECOUNT);
+			CopyNestedVec(saveDeltas, &deltas, MAXCAPTURECOUNT);
 			int l = 0;
 			for( int i = 0; i < samples.size()-1; i++ ) {
 				*( saveLabels.data() + l++ ) = float(labels[i][0]);
 				*( saveLabels.data() + l++ ) = float(labels[i][1]);
 			}
-			if (readSamples.size() > 0 && readLabels.size() > 0) {
-				MatrixXf outData = MatrixXf(readSamples.rows() + saveData.rows(), saveData.cols());
-				outData << readSamples, saveData;
-				MatrixXf outLabels = MatrixXf(readLabels.rows() + saveLabels.rows(), saveLabels.cols());
+			if( readSamples.size() + readLabels.size() + readDeltas.size() > 0 ) {
+				MatrixXf outSamples = MatrixXf(readSamples.rows(), readSamples.cols() + saveSamples.cols());
+				outSamples << readSamples, saveSamples;
+				MatrixXf outDeltas = MatrixXf(readDeltas.rows(), readDeltas.cols() + saveDeltas.cols());
+				outSamples << readDeltas, saveDeltas;
+				MatrixXf outLabels = MatrixXf(readLabels.rows(), readLabels.cols() + saveLabels.cols());
 				outLabels << readLabels, saveLabels;
-				writeToCSVfile("GestureData.csv", outData);
-				writeToCSVfile("GestureLabels.csv", outLabels);
+				writeToCSVfile("GestureSamples.csv", outSamples.transpose());
+				writeToCSVfile("GestureDeltas.csv", outDeltas.transpose());
+				writeToCSVfile("GestureLabels.csv", outLabels.transpose());
 			}
 			else {
-				writeToCSVfile("GestureData.csv", saveData);
-				writeToCSVfile("GestureLabels.csv", saveLabels);
+				writeToCSVfile("GestureSamples.csv", saveSamples.transpose());
+				writeToCSVfile("GestureDeltas.csv", saveDeltas.transpose());
+				writeToCSVfile("GestureLabels.csv", saveLabels.transpose());
 			}
 		} else {
-			writeToCSVfile("GestureLabels.csv", readLabels);
+			writeToCSVfile("GestureLabels.csv", readLabels.transpose());
 		}
 		DeleteDC(deviceContext);
 	}
