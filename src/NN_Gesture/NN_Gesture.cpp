96c7c89d925dfefc52cf8d7e686337e1a40a8706
Master <jlecoui@msn.com>
2020-08-15 02:28:25 -0700

    Revert "Const Ref; json read activation fix; Header ifndefs; removed Cats"
    This reverts commit d183c37bd4afcbac793a4bae67ecec80c71af5ff.

--- src/NN_Gesture/NN_Gesture.cpp
+++ src/NN_Gesture/NN_Gesture.cpp
@@ -294,7 +294,7 @@ int CALLBACK WinMain(HINSTANCE Instance, HINSTANCE PrevInstance, LPSTR CommandLi
 		while( globalRunning && steps < 500) {
 			Win32ProcessPendingMessages();
 			if( isTraining ) {
-				for( int e = 0; e < 1; e++ ) {
+				for( int e = 0; e < 100; e++ ) {
 					trainer.UpdateSingleStep();
 				}
 				UpdateHistory(history);
