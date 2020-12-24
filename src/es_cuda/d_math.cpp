c5e7235b330409080a6d0955c32e60829f1fe9c7
Master <jlecoui@msn.com>
2020-12-23 19:10:32 -0800

    Fix merge

--- src/es_cuda/d_math.cpp
+++ src/es_cuda/d_math.cpp
@@ -254,15 +254,7 @@ void Sigmoid_Kernal(float *dst, int m, int k) {
 	int col = GetCol();
 	int tid = col * m + row;
 	if (col < k && row < m) {
-<<<<<<< HEAD
 		dst[tid] = __fdividef(1.f, (__fadd_rd(1.f, __expf(-dst[tid]))));
-=======
-#if FASTER
-		dst[tid] = __fdividef(1.f, (__fadd_rd(1.f, __expf(-dst[tid]))));
-#else
-		dst[tid] = 1.f / (1.f + exp(-dst[tid]));
-#endif
->>>>>>> master
 	}
 }
 __global__
